b60a265f7b6c4b358db469d05b61d6ef34ce369c
Komal2006 <komaldeepkr99@gmail.com>
2019-10-18 18:25:49 -0400

    Initial Commit

---
+++ day8_closures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  day8_closures
+//
+//  Created by MacStudent on 2019-10-18.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
